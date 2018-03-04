a2603b3d925493281f7dfd9055c8184c52bb65cd
Solomon <solomona2003@yahoo.com>
2018-03-04 17:46:32 +0100

    playing arround with MD components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [MatIconModule, MatMenuModule, MatCheckboxModule, MatButtonModule, MatTabsModule, MatProgressSpinnerModule ],
+    exports: [MatIconModule, MatMenuModule, MatCheckboxModule, MatButtonModule, MatTabsModule, MatProgressSpinnerModule]
 
 })
 
